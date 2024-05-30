16d38c7ffb026c3859a99623145a6551a461127f
shmanmantech <shmanmantech@gmail.com>
2024-05-30 16:10:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express ();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("I'm listening in port:", PORT);
+});
+
+app.get("/checkstatus", (req, res) => {
+   const status = {
+      "Status": "Alive"
+   };
+   
+   res.send(status);
+});
